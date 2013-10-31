acbb3072f40e22713c65221d06a16302ff1d0497
Irfan Mahfudz Guntur <>
2013-11-01 01:24:09 +0800

    Initial commit

--- application/views/front/beranda_view.php
+++ application/views/front/beranda_view.php
@@ -51,11 +51,8 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1>Hello, world!</h1>
-         <?php if(isset($error))echo $error; ?>
-          <?php echo form_error('email');?>
-<?php echo form_error('password');?>rtert
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <h1>Katharina Ratu Dewi</h1>      
+        <p>To hire Katharina Ratu Dewi or to get pricing, fees and availability on Katharina Ratu Dewi to perform at your corporate event, gala, fundraising event, private performance, party, fundraiser, public concert, wedding, college, exhibition or festival worldwide. Fill out and submit the simple entertainment request form, and a booking agent for Katharina Ratu Dewi will get back to you in under 24 hours to discuss the artists pricing, fees and availability.</p>
         <p><a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
       </div>
     </div>
