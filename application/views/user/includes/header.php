6b7d367b240adfa1c224ae9f24f343ceacdfccad
Irfan Mahfudz Guntur <ayes@bsmsite.com>
2013-12-19 21:44:34 +0800

    Initial commit

--- application/views/user/includes/header.php
+++ application/views/user/includes/header.php
@@ -21,9 +21,8 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
-   
   </head>
-
   <body>
+ <?php date_default_timezone_set("Asia/Jakarta");?>
 
     
